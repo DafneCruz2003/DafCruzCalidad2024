46d0df7f04ed2329487e64f1188ac3ba67fc7527
DafneCruz2003 <comprasfamiliares24@gmail.com>
2024-11-24 14:30:03 -0600

    comentario en class

--- src/main/java/com/fca/calidad/MyClass.java
+++ src/main/java/com/fca/calidad/MyClass.java
@@ -2,6 +2,7 @@ package com.fca.calidad;
 
 public class MyClass {
     public String getClassNameUpperCase() {
+    	//solo puse el meotodo¿
         // es para el nombre de la clase en mayúsculas (o lo que se pruebe en el mock)
         return this.getClass().getSimpleName().toUpperCase();
     }
